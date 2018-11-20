b20e3ea1ff9d78210b2f215c45191db491cc25dc
Lemonsity <38927629+Lemonsity@users.noreply.github.com>
2018-11-20 08:57:14 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,7 @@
 //This is a class that help me test feature of the Java language
 //Test
 public class Test {
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
 }
